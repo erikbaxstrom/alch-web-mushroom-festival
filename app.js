dd0b0d8baad1cad21e038d3c7bf6de7f60fc326d
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-19 14:21:29 -0700

    implement displayMushrooms function

--- app.js
+++ app.js
@@ -91,10 +91,13 @@ function displayMessage() {
 
 function displayMushrooms() {
     mushroomContainer.innerHTML = '';
-
     // > loop the mushrooms
-    // create a mushroom element using the renderMushroom function
-    // append it to the container
+    for (let mushroom of mushrooms) {
+        // create a mushroom element using the renderMushroom function
+        const mushroomEl = renderMushroom(mushroom);
+        // append it to the container
+        mushroomContainer.append(mushroomEl);
+    }
 }
 
 function displayFriends() {
