df902e5da99ac4b87d472be54288e0e3f3b12d37
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-19 18:29:46 -0700

    implement feed freind when no mushrooms

--- app.js
+++ app.js
@@ -116,7 +116,9 @@ function displayFriends() {
         friendEl.addEventListener('click', () => {
             // > handle the three possible outcomes:
             // 1. No mushrooms, set a message to go hunt for more
-
+            if (!mushrooms.length) {
+                message = 'No mushrooms. Time to go mushroom hunting.';
+            }
             // 2. Friend is already fully satisfied (3), set a message to pick another friend
             // 3. Feed friend mushroom:
             // a. "pop" a mushroom off the mushrooms array
