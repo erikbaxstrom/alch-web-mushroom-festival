c2b05084960ce4c3a96f16ce70fefff9d209cbf2
Erik M Baxstrom <baxstrem@gmail.com>
2022-09-19 18:33:27 -0700

    implement no feeding of full friend

--- app.js
+++ app.js
@@ -118,13 +118,17 @@ function displayFriends() {
             // 1. No mushrooms, set a message to go hunt for more
             if (!mushrooms.length) {
                 message = 'No mushrooms. Time to go mushroom hunting.';
+            } else {
+                // 2. Friend is already fully satisfied (3), set a message to pick another friend
+                if (friend.satisfied >= 3) {
+                    message = `${friend.name} is already satisfied. Choose a different friend`;
+                }
+                // 3. Feed friend mushroom:
+                // a. "pop" a mushroom off the mushrooms array
+                // b. increase friend.satisfied by 1
+                // c. set a message that the friend enjoyed the mushroom,
+                //    include the friend name and mushroom type in the message
             }
-            // 2. Friend is already fully satisfied (3), set a message to pick another friend
-            // 3. Feed friend mushroom:
-            // a. "pop" a mushroom off the mushrooms array
-            // b. increase friend.satisfied by 1
-            // c. set a message that the friend enjoyed the mushroom,
-            //    include the friend name and mushroom type in the message
 
             displayMessage();
             displayMushrooms();
